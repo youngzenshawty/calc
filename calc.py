dff424e5ff701d375f1de9f6975e35cf5f25874a
youngzenshawty <66177612+youngzenshawty@users.noreply.github.com>
2020-05-30 11:15:27 -0600

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,14 @@
+num1 = float(input("Enter first number: "))
+Op = (input("Enter oporator: ")
+num2 = float(input("Enter first number: "))
+
+if Op == "+":
+    print(num1 + num2)
+elif Op == "-":
+    print(num1 - num2)
+elif Op == "/":
+    print(num1 / num2)
+elif Op == "*":
+    print(num1 * num2)
+else:
+    print ("you  ah stupid af boi")
